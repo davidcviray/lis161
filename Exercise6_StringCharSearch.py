5fe294b9405843e05cb2553f4b6e7ee0cb8e6a23
davidcviray <54297600+davidcviray@users.noreply.github.com>
2019-10-03 12:59:49 +0800

    Add files via upload

---
+++ Exercise6_StringCharSearch.py
@@ -0,0 +1,10 @@
+def count(word,char):
+    return word.count(char)
+
+string = input('Enter a word ')
+string.lower()
+character = (input('Enter a character '))
+character.lower()
+print(count(string,character))
+
+    
